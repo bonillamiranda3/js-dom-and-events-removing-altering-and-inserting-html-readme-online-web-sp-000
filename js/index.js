2d1307e484224c28788578ff83879aa344ef7b7e
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-03-25 01:25:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
 
 let ul = document.createElement('ul');
- 
+
 for (let i = 0; i < 3; i++) {
   let li = document.createElement('li');
   li.innerHTML = (i + 1).toString();
   ul.appendChild(li);
 }
- 
+
 element.appendChild(ul);
 
 
